fdad8503008e3825f715371228b675bdb10e7974
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:17:11 +0530

    Add files via upload
    A C program to calculate the perimeter of a square using its side length.

---
+++ perimeter_of_square.c
@@ -0,0 +1,16 @@
+//perimeter of square//
+
+#include<stdio.h>
+
+int main()
+{
+    float peri,side;
+
+    printf("Enter side to calculate perimetr of square : ");
+    scanf("%f",&side);
+
+    peri = 4 * side;
+
+    printf("Perimeter of square : %f",peri);
+
+}
