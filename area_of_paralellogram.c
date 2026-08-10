8e7262efa3870580f4c7b0b9988623ef1ee0f74a
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:11:53 +0530

    Add files via upload
    A C program to calculate the area of a parallelogram using base and height.

---
+++ area_of_paralellogram.c
@@ -0,0 +1,16 @@
+//Area of paralellogram//
+
+#include<stdio.h>
+
+int main()
+{
+    float base,height,area;
+
+    printf("Enter base and height : ");
+    scanf("%f%f",&base,&height);
+
+    area = base*height;
+
+    printf("Area of paralellogram : %f",area);
+
+}
