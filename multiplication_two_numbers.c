53474004f913a0dd06212388b3b5ff2f08b7bd35
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:05:53 +0530

    Add files via upload
    A basic C program to calculate the product of two numbers.
    Demonstrates the use of the * arithmetic operator.

---
+++ multiplication_two_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int num1,num2,sum;
+
+
+    printf("ENTER 2 NUMBER FOR MULTIPLICATION : ");
+    scanf("%d%d",&num1,&num2);
+
+
+    multi = num1*num2;
+
+
+    printf("MULTIPLICATION of %d and %d is %d",num1,num2,multi);
+}
