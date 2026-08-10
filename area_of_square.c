620ace47746fa79ceabe672b2c7d73e89c284df6
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:10:03 +0530

    Add files via upload
    A C program to calculate the area of a square using its side length.

---
+++ area_of_square.c
@@ -0,0 +1,16 @@
+//Area of square//
+
+#include<stdio.h>
+
+int main()
+{
+    int area,side;
+
+    printf("Enter side to calculate area of square : ");
+    scanf("%d",&side);
+
+    area = side*side;
+
+    printf("Area of square : %d",area);
+
+}
