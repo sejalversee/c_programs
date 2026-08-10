fe8506a5e4df580bdf446818880d75ecd8e66ffe
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:09:21 +0530

    Add files via upload
    A C program to calculate the area of a rectangle using length and width.

---
+++ area_of_rectangle.c
@@ -0,0 +1,16 @@
+//Area of rectangle//
+
+#include<stdio.h>
+
+int main()
+{
+    int area,length,width;
+
+    printf("Enter length and width : ");
+    scanf("%d%d",&length,&width);
+
+    area = length*width;
+
+    printf("Area of rectangle : %d",area);
+
+}
