4e8e48ddaae1a3e5191a981abb2a0cb1da926de0
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 00:58:16 +0530

    Add files via upload
    A basic C program that prints personal details to the console.

---
+++ print_details.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("NAME : Sejal Naik");
+    printf("\nAGE : 20");
+    printf("\nCITY : Pune");
+    printf("\nCOURSE : CS");
+    printf("\nDEGREE : BSC(computer science)");
+    printf("\nUNIVERSITY : SPPU Pune");
+    printf("\nCOLLEGE : RJSPM");
+    printf("\nFULL STACK DEVELOPER");
+    printf("\nLINKCODE");
+}
