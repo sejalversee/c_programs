4f2f90abe85548f7b607e1f499eda66b64230e61
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:20:18 +0530

    Add files via upload
    A C program to calculate the perimeter of a rhombus using its side length.

---
+++ perimeter_of_rhombus.c
@@ -0,0 +1,22 @@
+//perimeter of Rhombus //
+
+
+#include<stdio.h>
+
+
+int main()
+{
+    float peri,side;
+
+
+    printf("Enter side to calculate perimetr of Rhombus : ");
+    scanf("%f",&side);
+
+
+    peri = 4 * side;
+
+
+    printf("Perimeter of Rhombus : %f",peri);
+
+
+}
