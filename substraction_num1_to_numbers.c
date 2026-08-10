fa7bb91a7de3adc495327eae17a21a0a1bfd8cdf
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-11 01:01:20 +0530

    Add files via upload
    A basic C program to calculate the difference between two numbers.
    Demonstrates the use of the - arithmetic operator.

---
+++ substraction_num1_to_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int num1,num2,sub;
+
+
+    printf("ENTER 2 NUMBER FOR SUBSTRACTION : ");
+    scanf("%d%d",&num1,&num2);
+
+
+    sub = num1-num2;
+
+
+    printf("Substraction from %d to %d is %d",num1,num2,sum);
+}
