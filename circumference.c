c67eb8104a2d8555d318f82999272dad9e7ec62f
Sejal Naik <sejalnaik0602@gmail.com>
2026-08-14 22:05:22 +0530

    Add files via upload
    program to find circumference

---
+++ circumference.c
@@ -0,0 +1,21 @@
+//Circumference//8
+
+#include<stdio.h>
+
+int main()
+{
+    float pi = 3.14,r,circumference;
+
+    printf("Enter radius to calculate Circumference : ");
+    scanf("%f",&r);
+
+    circumference = 2 * pi * r;
+
+    printf("circumference : %f",circumference);
+    
+
+    
+
+    
+
+}
